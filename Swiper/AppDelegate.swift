07609dc3b6c019c2591811263ff285d60cc867e2
Emily Brenneke <brenneke@gmail.com>
2016-04-20 20:07:17 -0500

    comment out fabric line since key doesn't exist in this branch

--- Swiper/AppDelegate.swift
+++ Swiper/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var motionDelegate: ReactToMotionEvent?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Fabric.with([Crashlytics.self])
+//        Fabric.with([Crashlytics.self])
 
         let notificationCenter = NSNotificationCenter.defaultCenter()
         notificationCenter.addObserver(self, selector: .setupControllers, name: GCControllerDidConnectNotification, object: nil)
