90834693910eafe061c5067660addb6636f6d3ac
Emily Brenneke <brenneke@gmail.com>
2016-08-10 21:14:11 -0500

    changed let cases, who knew?

--- Swiper/GameViewController.swift
+++ Swiper/GameViewController.swift
@@ -26,31 +26,29 @@ class GameViewController: UIViewController {
                 Crashlytics.self().recordError(error)
                 print(error)
             } else {
-                if let contentsURL = self.resourceRequest?.bundle.URLForResource("contents", withExtension: "json") {
-                    if let contentsData = NSData(contentsOfURL: contentsURL) {
-                        if let contents = try? NSJSONSerialization.JSONObjectWithData(contentsData, options: .AllowFragments) as? Array<[String:String]> {
-                            NSOperationQueue.mainQueue().addOperationWithBlock({ 
-                                if let scene = GameScene(fileNamed: "GameScene"), contents = contents where contents.count > 0 {
-                                    let index = Int(arc4random_uniform(UInt32(contents.count)))
-                                    scene.imageName = contents[index]["filename"]! // TODO: get rid of !
-                                    scene.difficulty = self.difficulty
+                if let contentsURL = self.resourceRequest?.bundle.URLForResource("contents", withExtension: "json"),
+                    contentsData = NSData(contentsOfURL: contentsURL),
+                    contents = try? NSJSONSerialization.JSONObjectWithData(contentsData, options: .AllowFragments) as? Array<[String:String]> {
+                    NSOperationQueue.mainQueue().addOperationWithBlock({
+                        if let scene = GameScene(fileNamed: "GameScene"), contents = contents where contents.count > 0 {
+                            let index = Int(arc4random_uniform(UInt32(contents.count)))
+                            scene.imageName = contents[index]["filename"]! // TODO: get rid of !
+                            scene.difficulty = self.difficulty
 
-                                    // Configure the view.
-                                    let skView = self.view as! SKView
-                                    skView.showsFPS = true
-                                    skView.showsNodeCount = true
+                            // Configure the view.
+                            let skView = self.view as! SKView
+                            skView.showsFPS = true
+                            skView.showsNodeCount = true
 
-                                    /* Sprite Kit applies additional optimizations to improve rendering performance */
-                                    skView.ignoresSiblingOrder = true
+                            /* Sprite Kit applies additional optimizations to improve rendering performance */
+                            skView.ignoresSiblingOrder = true
 
-                                    /* Set the scale mode to resize to fit the window */
-                                    scene.scaleMode = .ResizeFill
+                            /* Set the scale mode to resize to fit the window */
+                            scene.scaleMode = .ResizeFill
 
-                                    skView.presentScene(scene)
-                                }
-                            })
+                            skView.presentScene(scene)
                         }
-                    }
+                    })
                 }
             }
         })
